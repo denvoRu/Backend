d7f798efcc14ea6cc4d25f4547e608f5444d8dd5
tarkue <mrfrostlikchannel@gmail.com>
2025-01-10 18:20:27 +0500

    code review

--- src/domain/services/module_service.py
+++ src/domain/services/module_service.py
@@ -46,7 +46,7 @@ async def get_all_with_subjects(
             institute_ids,
             teacher_ids
         )
-    except Exception as e:
+    except Exception:
         raise HTTPException(
             status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
             detail="One or more parameters are invalid"
