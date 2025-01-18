71f56321a3b6a1b8227a090e240a1235199394e5
tarkue <mrfrostlikchannel@gmail.com>
2025-01-18 16:34:23 +0500

    fix

--- src/application/dto/lesson/add_lesson_dto.py
+++ src/application/dto/lesson/add_lesson_dto.py
@@ -9,8 +9,8 @@ from datetime import time, date as dateType
 class AddLessonDTO(BaseModel):
     # lesson data transfer add fields
     subject_id: UUID4 = Field(description="id of subject", examples=['d216bd55-4f57-40fa-a6d1-8444f43ccacf'])
-    lesson_name: str = Field(min_length=10, max_length=100, examples=["Разработка на Python (FastAPI)", "Javascript (React)"])
-    speaker_name: str = Field(min_length=10, max_length=100, examples=["Смирнов Евгений Сергеевич", "Иванов Иван Иванович"])
+    lesson_name: str = Field(min_length=2, max_length=100, examples=["Разработка на Python (FastAPI)", "Javascript (React)"])
+    speaker_name: str = Field(min_length=2, max_length=100, examples=["Смирнов Евгений Сергеевич", "Иванов Иван Иванович"])
     date: dateType = Field(description="date in format YYYY-MM-DD", examples=["2023-01-01", "2023-01-02"])
     start_time: time = Field(description="time in format HH:MM", examples=["12:00", "13:00"])
     end_time: time = Field(description="time in format HH:MM", examples=["13:30", "15:23"])
