5702cce166e71d36e833afc4f277d5c64f839568
tarkue <mrfrostlikchannel@gmail.com>
2025-01-19 14:59:20 +0500

    fix

--- src/infrastructure/repositories/study_group_repository/get_study_group.py
+++ src/infrastructure/repositories/study_group_repository/get_study_group.py
@@ -48,7 +48,7 @@ async def get_const_links(
         filters.append(or_(teacher_name, module_name, subject_name))
 
     stmt = select(
-        distinct(StudyGroup.id.label("id")),
+        StudyGroup.id.label("id"),
         Module.id.label("module_id"),
         Module.name.label("module_name"),
         Subject.id.label("subject_id"),
